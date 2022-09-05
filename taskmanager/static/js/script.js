40da309ca0321848ea8812bd941656be1a92d3c2
Vilayat Kleer <vkleer@hotmail.com>
2022-09-05 12:07:41 +0000

    Add delete functionality with modal confirmation

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+    // modal initialization
+    let modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
 });
