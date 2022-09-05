5940b48ea6ade1fbd662ddcfc9e633e76ce92e1e
Vilayat Kleer <vkleer@hotmail.com>
2022-09-05 10:05:47 +0000

    Add database schemas and populate database with them

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -5,16 +5,25 @@ class Category(db.Model):
     # Schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __rept__ to represent itself in the form of a string
-        return self
+        return self.category_name
 
 
 class Task(db.Model):
     # Schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
+    is_urgent = db.Column(db.Boolean, default=False, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    category_id = db.Column(
+        db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"),
+        nullable=False
+    )
 
     def __repr__(self):
         # __rept__ to represent itself in the form of a string
-        return self
+        return f"#{self.id} - Task: {self.task_name} | Urgent: {self.is_urgent}"
