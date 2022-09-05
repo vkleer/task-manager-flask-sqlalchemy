a42b6c1069876e3078531b2dc32e5b4dd9e6ff91
Vilayat Kleer <vkleer@hotmail.com>
2022-09-05 12:41:55 +0000

    Add task adding functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,7 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
     // modal initialization
     let modals = document.querySelectorAll('.modal');
     M.Modal.init(modals);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // Select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
